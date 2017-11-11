1a378feadda71bc50f6318b79b2e21f49431ded6
solufemi91 <stevenoo@spartaglobal.com>
2017-11-11 20:19:23 +0000

    boxes change colors with button

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function(event) {
+
+var $startButton = $('#startButton');
+var $zeroBox = $('#zero');
+var $oneBox = $('#one');
+var $twoBox = $('#two');
+var $threeBox = $('#three');
+var $boxes = $('.boxes')
+var $fourColours = ['red','blue','green','yellow']
+var i = 0;
+$startButton.click(function(){
+
+  $zeroBox.css('background-color','red');
+  $oneBox.css('background-color','blue');
+  $twoBox.css('background-color','green');
+  $threeBox.css('background-color','yellow');
+
+  // var id = setInterval(frame,1000);
+  // function frame(){
+  //   $boxes[i].css('background-color','')
+  //   i++;
+  // }
+
+
+
+
+})
+
+
+});
